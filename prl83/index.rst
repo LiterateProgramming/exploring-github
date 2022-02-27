4dd70cb79fb430419525c7b9003c4a9ced86703f
Peter Loux <prl83@msstate.edu>
2022-02-27 16:46:26 -0600

    add prl83 subdirectory with initial code

---
+++ prl83/index.rst
@@ -0,0 +1,18 @@
+**********************************
+Peter Loux's Code
+**********************************
+
+.. toctree::
+   :maxdepth: 2
+
+
+   C++ solution by Kaitlyn Jackson <Thor_Puzzle_assign.c>
+   Python 3 solution by Peter Loux <thor.py>
+   feedback
+
+.. toctree::
+    :hidden:
+
+
+
+.. comment
---
+++ prl83/thor.py
@@ -0,0 +1,163 @@
+# ******************************
+# Power of Thor - Episode 1
+# ******************************
+# -------------
+# Introduction
+# -------------
+# This documentation will walk you through developing a Python 3 solution to the `Power of Thor - Episode 1 puzzle <https://www.codingame.com/ide/puzzle/power-of-thor-episode-1>`_. What's more, this whole document can just be copy-pasted into the solution window to solve the puzzle if you just want to cheat and not learn anything.
+#
+# ------------------
+# Procedure and Tips
+# ------------------
+# So what do I have to do?
+# ====================================
+# .. image:: thor_1.png
+#   :width: 100%
+#
+# The little man is Thor. The red lightning is the "light of power". You have to make Thor walk to the light of power.
+#
+# How do I make him walk?
+# =======================
+# With ``print()`` statements. There are eight valid directions: "N", "NE", "E", "SE", "S", "SW", "W", and "NW". So if you run ``print("N")``, Thor will take one step "up".
+#
+# .. image:: directions.png
+#   :width: 50%
+#
+# How do I make him go towards the light?
+# =======================================
+#
+# Thor moves on a map which is 40 wide by 18 high. Note that the coordinates (X and Y) start at the top left! This means the most top left cell has the coordinates "X=0,Y=0" and the most bottom right one has the coordinates "X=39,Y=17".
+# You're given four variables: ``light_x`` and ``light_y`` give the x and y coordinates of the light. They never change. The variables ``initial_tx`` and ``initial_ty`` are the *starting* position of Thor. They never change either. **You need to make two new variables for Thor's current position.** Give them *meaningful* names, like ``thor_x`` and ``thor_y``.
+#
+# .. image:: moving.png
+#   :width: 100%
+#
+# See, if Thor is at (3,6), and the light is at (3,8), you want to go south. So you ``print("S")`` and he takes one step south, and is now at (3,7). You don't have to do anything when you get to the light; you automatically win.
+# Sounds easy enough, but what about all that gibberish in the solution window?
+#
+#
+# .. image:: starter_code.png
+#   :width: 100%
+#
+# That *is* confusing. Here:
+#
+# .. image:: new_starter.png
+#   :width: 100%
+#
+# Ok, now there's less of it. What does it do?
+# ===========================================================
+#
+# The first two lines import libraries that you need.
+#
+# The third line uses the split() method to parse input from the game. You can read more about ``split`` `here <https://www.tutorialspoint.com/python3/string_split.htm>`_. Basically the game starts you with a string like "3 8 3 6" and this line takes that string and sets the light position variables to (3,8) and Thor's initial position to (3,6).
+#
+# Then there's an infinite while loop. Whatever you want Thor to do each turn, you put it in the loop.
+#
+# The final line keeps track of how many turns you have left.
+#
+# What should I do first?
+# =======================
+#
+# Start outside the while loop and set Thor's current position to his initial position like this:
+#
+# .. image:: initial.png
+#   :width: 50%
+#
+# Then what?
+# ==========
+#
+# Each turn, compare Thor's *current* position to the light's position. Figure out which direction he needs to go and ``print`` it. Then don't forget to update thor_x to reflect where you moved him. Like this:
+#
+# .. image:: first_step.png
+#   :width: 50%
+#
+# Let's goooo!!
+# =============
+#
+# You should take a minute or two to plan it out. I drew this little sketch to help me:
+#
+# .. image:: sketch.png
+#   :width: 100%
+#
+# Wait, what if I want him to move a different direction besides NE?
+# ==================================================================
+#
+# You could use an ``elif`` which is python for "else if". Here, I did another one for you:
+#
+# .. image:: next_step.png
+#   :width: 100%
+#
+# That looks simpler. Why don't I just move him one of the four cardinal directions each turn?
+# ============================================================================================
+#
+# That *would* be less typing, but when I did it, I ran out of moves. You can try it if you want.
+#
+# How do I screw it up?
+# =====================
+#
+# If you move in the wrong direction, you'll go off the map and lose. You'll also lose if Thor just sits there too long.
+#
+# How can I debug my code by having it print stuff in the console?
+# ================================================================
+#
+# To debug: ``print("Debug messages...", file=sys.stderr, flush=True)``
+#
+# Is there another way to solve it?
+# =================================
+#
+# Yes, in Python, you can append to strings with the ``+`` operator. So make a variable called ``thor_move``, and set it to an empty string each turn. If he needs to go north, append "N" to it. If he also needs to move east, append "E" to it. Then at the end of the turn, just ``print(thor_move)``
+#
+# -------------
+# Solution
+# -------------
+#
+## Don't change this #######################################################
+import sys
+import math
+light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+############################################################################
+
+thor_x = initial_tx
+thor_y = initial_ty
+
+######## game loop - don't change this either ####################################
+while True:
+    remaining_turns = int(input())  # The remaining amount of turns Thor can move.
+##################################################################################
+
+    if thor_x < light_x and thor_y > light_y:
+        print ("NE")
+        thor_x += 1
+        thor_y -= 1
+
+    elif  thor_x < light_x and thor_y < light_y:
+        print ("SE")
+        thor_x += 1
+        thor_y += 1
+
+    elif thor_x > light_x and thor_y < light_y:
+        print ("SW")
+        thor_x -= 1
+        thor_y += 1
+
+    elif thor_x > light_x and thor_y > light_y:
+        print ("NW")
+        thor_x -= 1
+        thor_y -= 1
+
+    elif thor_y > light_y:
+        print ("N")
+        thor_y -= 1
+
+    elif thor_x < light_x:
+        print ("E")
+        thor_x += 1
+
+    elif thor_y < light_y:
+        print ("S")
+        thor_y += 1
+
+    elif thor_x > light_x:
+        print("W")
+        thor_x -= 1
+
