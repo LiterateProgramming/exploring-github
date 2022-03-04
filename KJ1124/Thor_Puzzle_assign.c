b4da550289a8ef3ad571df3342613c0c3c20a5a6
Kahjax <kj1124@msstate.edu>
2022-03-04 00:48:31 -0600

    More small edits

--- KJ1124/Thor_Puzzle_assign.c
+++ KJ1124/Thor_Puzzle_assign.c
@@ -8,7 +8,7 @@ Use `this link <https://www.codingame.com/ide/puzzle/power-of-thor-episode-1>`_ 
 General Instructions
 --------------------
 
-**Objective:**
+**Objective**
 
 Write a program that will guide Thor to reach the light of power in the most efficient manner.
 
@@ -82,6 +82,7 @@ The directions below are the variables used to output the direction in which you
 Thor's Movement
 ----------------
 */
+
         /* In this case the light of power at the X and Y coordinates are greater than Thor's initial condition so 2 if condition statements have to be implemented. Thor's initial X and Y conditions must be incremented so he can move in a diagonal direction without going out of bounds or repositioning from the light of power.
         */
         if ((light_y > initial_ty) && (light_x > initial_tx))
@@ -89,29 +90,29 @@ Thor's Movement
             cout << "SE" << endl;
             initial_tx++;
             initial_ty++;
-        }
+            }
         /* Based on the compass it can be determined that Thor will travel E (East).The light_x is the position of the light of power  and initial_tx is Thor's starting position in the X-coordinate plane. The postion of the light of power is is going in the positive direction and it greater than Thor's starting postion so a condition statement is created that if this is the case travel East.
         */
         else if (light_x > initial_tx) {
             cout << "E" << endl;
-        }
+            }
         /* Thor will travel N (North) since the light of power is above his location and it is in the positive direction .The light_y is the position of the light of power and initial_ty is Thor's starting position in the Y-coordinate plane.
         */
         else if (light_y < initial_ty) {
             cout << "N" << endl;
-        }
+            }
         /* In this instance the light of power is in the negative Y direction of Thor's position. It is also at an angle which means Thor must move in an diagonal direction so that he does not run out of energy. He will travel diagional because of the increment then straight.
         */
         else if (light_y > initial_ty) {
             cout << "SW" << endl;
             initial_ty++;
-        // Positive increment here so that Thor does not run out of bounds.
-        }
+            // *Positive increment here so that Thor does not run out of bounds.*
+            }
+        // This else if statement is so that Thor will travel West after incrementing to the point where the light of power is equal to Thor's initial position.
         else if (light_x < initial_tx) {
             cout << "W" << endl;
-        // This else if statement is so that Thor will travel West after incrementing to the point where the light of power is equal to Thor's initial position.
-        } 
+
+            } 
     }
-}
 
 
