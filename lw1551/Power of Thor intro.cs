f1d3072e73dd058460d95dc41c5a4f49d526c759
LoganWilliams <99890268+DisarmedRemo66@users.noreply.github.com>
2022-03-07 19:30:29 -0600

    Assignment Revisions 3-7-22

--- lw1551/Power of Thor intro.cs
+++ lw1551/Power of Thor intro.cs
@@ -1,7 +1,7 @@
 // *************
 // Power of Thor
 // *************
-// This is my solution for the `Power of Thor coding game <https://www.codingame.com/training/easy/power-of-thor-episode-1/solution>`_
+// This is my solution for the `Power of Thor <https://www.codingame.com/training/easy/power-of-thor-episode-1/solution>`_
 //
 //
 // =====================
@@ -9,7 +9,7 @@
 // =====================
 // * Thor needs to be able to move based on the different locations of the power orb.
 // * Thor currently just walks off the screen and goes out of bounds due to being stuck in a while loop that does not check to see thor's location in relation to either the light or the border of the screen.
-// * There is going to need to be a way to increment Thor’s location in at least two variables depending on the cardinal direction he \moves in.
+// * There is going to need to be a way to increment Thor’s location in at least two variables depending on the cardinal direction he moves in.
 // * Make a variable that is Thor’s updated location after he moves from his initial location, then change this variable based on checking where thor is in relation to the power orb.
 // * Use *if statements* to check for thor’s updated location compared to the orb, then use *else statements* to compare them for each direction that thor can move in.
 // 
@@ -29,57 +29,58 @@
 // ---------------------
 //
         string[] inputs = Console.ReadLine().Split(' ');
-        int lightX = int.Parse(inputs[0]); 
         // The X position of the light of power
-        int lightY = int.Parse(inputs[1]); 
+        int lightX = int.Parse(inputs[0]); 
         // The Y position of the light of power
-        int initialTX = int.Parse(inputs[2]); 
+        int lightY = int.Parse(inputs[1]); 
         // Thor's starting X position
-        int initialTY = int.Parse(inputs[3]); 
+        int initialTX = int.Parse(inputs[2]); 
         // Thor's starting Y position
- //
-        // The first two variables, lightX and lightY, are used to set where the coordinates of the light orb's location for each new level of the game. I then defined initialTX and initialTY to be Thor's initial location. When first designing this program, I had two addtional variables that would update to thor's new location, but I realized when working through this that the initialTX and initialTY variables can be used for both functions.
-//
-//
-//
+        int initialTY = int.Parse(inputs[3]); 
+        // The first two variables, ``lightX`` and ``lightY``, are used to set where the coordinates of the light orb's location for each new level of the game. I then defined ``initialTX`` and ``initialTY`` to be Thor's initial location. When first designing this program, I had two addtional variables that would update to thor's new location, but I realized when working through this that the ``initialTX`` and ``initialTY`` variables can be used for both functions.
         string directionX = "";
         // Blank X direction string to fill with direction thor will move in.
         string directionY = "";
         // Blank Y direction string to fill with direction thor will move in.
         // These variables are used to read in the cardinal direction given by the player of the game.
-
+//
 // ****************
 // Solution
 // ****************
-// This code operates by making multiple comparisons each time Thor moves. The loop begins by establishing that thor has a X and a Y direction, and creating a string variable that will store the Direction in it. For example, if Thor needs to move to the bottom right, then the if statements will check for whether Thor's X location is larger than the light's X value. The same will be done for the Y values of the game. Therefore after several runs through the code you will be moving in all cardinal directions. So the basic idea is that the strings will tell which cardinal direction we want to go in, while the code itself will run comparisons between thor's orginal/updated location and the light's location to decide if the move is valid for the game to be won. 
-            while (true)
-        {
-            int remainingTurns = int.Parse(Console.ReadLine()); 
-            // The remaining amount of turns Thor can move. Do not remove this line.
-
-            string directionX = "";
-            // Blank X direction string to fill with direction thor will move in.
-            string directionY = "";
-            // Blank Y direction string to fill with direction thor will move in.
-
-            if (initialTX > lightX)
+        // This code operates by making multiple comparisons each time Thor moves. The loop begins by establishing that thor has a X and a Y direction, and creating a string variable that will store the Direction in it. For example, if Thor needs to move to the bottom right, then the if statements will check for whether Thor's X location is larger than the light's X value. The same will be done for the Y values of the game. Therefore after several runs through the code you will be moving in all cardinal directions. So the basic idea is that the strings will tell which cardinal direction we want to go in, while the code itself will run comparisons between thor's orginal/updated location and the light's location to decide if the move is valid for the game to be won. 
+        //
+        // .. image:: PowerOfThorXY.png
+        while (true)
             {
-                directionX = "W";
-                initialTX--;
-            }
-            else if(initialTX < lightX)
-            {
-                directionX = "E";
-                initialTX++;
-            }
-
-            if(initialTY > lightY)
-            {
-                directionY = "N";
-                initialTY--;
-            }
-            else if(initialTY < lightY)
-            {
-                directionY = "S";
-                initialTY++;
-            }
+                // The remaining amount of turns Thor can move. Do not remove this line.
+                int remainingTurns = int.Parse(Console.ReadLine()); 
+                // Blank X direction string to fill with direction thor will move in.
+                string directionX = "";
+                // Blank Y direction string to fill with direction thor will move in.
+                string directionY = "";
+                // I used if/else statements to work through each cardinal direction and insert a value for each of the strings above. These values are then used in the function below to move Thor in the user's desired direction.
+                //
+                // In order to represent East and West, we use the Horizontal of the game, which in terms of an XY coordinate system is left and right. The picture above shows the XY planes.
+                if (initialTX > lightX)
+                {
+                    directionX = "W";
+                    initialTX--;
+                }
+                else if(initialTX < lightX)
+                {
+                    directionX = "E";
+                    initialTX++;
+                }
+                // In order to represent North and South, we use the Vertical of the game, which in terms of an XY coordinate system is up or down. The picture above shows the XY planes.
+                if(initialTY > lightY)
+                {
+                    directionY = "N";
+                    initialTY--;
+                }
+                else if(initialTY < lightY)
+                {
+                    directionY = "S";
+                    initialTY++;
+                }
+                //Console.WriteLine("SE"); // A single line providing the move to be made: N NE E SE S SW W or NW
+                Console.WriteLine("{0}{1}",directionY,directionX);
